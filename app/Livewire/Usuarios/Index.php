dff4be8a11095d5ac4d19da6f1ab260f607f73a6
Gaia28 <matheusgaia33@gmail.com>
2025-12-02 10:14:53 -0300

    feat: barra de pesquisa funncionando + tabela dinamica

--- app/Livewire/Usuarios/Index.php
+++ app/Livewire/Usuarios/Index.php
@@ -3,13 +3,25 @@
 namespace App\Livewire\Usuarios;
 
 use Livewire\Attributes\Layout;
+use App\Models\User;
 use Livewire\Component;
 
 #[Layout('layouts.app')]
 class Index extends Component
 {
+    public string $search = '';
+
+    public function getUsuariosProperty()
+    {
+        return User::where('name', 'like', '%' . $this->search . '%')
+            ->orWhere('email', 'like', '%' . $this->search . '%')
+            ->orderBy('name')
+            ->get();
+    }
     public function render()
     {
-        return view('livewire.usuarios.index');
+        return view('livewire.usuarios.index', [
+            'usuarios' => $this->usuarios,
+        ]);
     }
 }
